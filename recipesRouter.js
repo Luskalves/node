42b9b99097ccc2d34c860fe932ca528cd5a35f00
Luskalves <lusca.alves32@gmail.com>
2022-06-15 11:14:35 -0300

    criando o rourter e adcionando algumas funções

---
+++ recipesRouter.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+
+const recipes = [
+  { id: 1, name: 'Lasanha', preco: 40.0, tempoDePreparo: 30 },
+  { id: 2, name: 'Macarrão a Bolonhesa', preco: 35.0, tempoDePreparo: 25 },
+  { id: 3, name: 'Macarrão com molho branco', preco: 35.0, tempoDePreparo: 25 },
+];
+
+function validateNames(req, res, next) {
+  const { name } = req.body;
+  if (!name || name === '') return res.status(400).json({ message: 'Invalid data'});
+
+  next();
+}
+
+router.get('/', (req, res) => {
+  res.status(200).json(recipes);
+});
+
+router.get('/', (req, res) => {
+  const { name, maxPrice } = req.query;
+  const fillteredRecipes = recipes.filter((r) => r.name.includes(name) 
+    && r.preco <= maxPrice);
+  
+  res.status(200).json(fillteredRecipes);
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const recipe = recipes.findIndex((r) => r.id === Number(id));
+  if (!recipe) return res.status(404).json({ message: 'Recipe not found! '})
+
+  res.status(200).json(recipe);
+})
+
+router.post('/', validateNames, (req, res) => {
+  const { id, name, price } = req.body;
+  recipes.push({ id, name, price });
+
+  res.status(201).json({ message: 'Recipe created successfully' });
+})
