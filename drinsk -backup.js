c7d374686544f2425a8fb3ea0fa68971957a1d8e
Luskalves <lusca.alves32@gmail.com>
2022-06-15 11:32:16 -0300

    criando o backup do drink

---
+++ drinsk -backup.js
@@ -0,0 +1,64 @@
+const drinks = [
+	{ id: 1, name: 'Refrigerante Lata', price: 5.0 },
+	{ id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+	{ id: 3, name: 'Suco 300ml', price: 4.0 },
+	{ id: 4, name: 'Suco 1l', price: 10.0 },
+	{ id: 5, name: 'Cerveja Lata', price: 4.5 },
+	{ id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+];
+
+app.get('/drinks', (_req, res) => {
+  res.json(drinks.sort((a,b) => a.name.localeCompare(b.name)));
+})
+
+app.get('/drinks/search', (req, res) => {
+  const { name } = req.query;
+  const filteredDrinks = drinks.filter((d) => d.name.includes(name));
+  res.status(200).json(filteredDrinks);
+})
+
+app.get('/drinks/:id', (req, res) => {
+  const { id } = req.params;
+  const drink = drinks.find((d) => d.id === Number(id));
+
+  if (!drink) return res.status(404).json({ message: 'Drink not found!' });
+
+  res.status(200).json(drink)
+})
+
+app.get('/validateToken', function (req, res) {
+  const token = req.headers.authorization;
+  if (token.length !== 16) return res.status(401).json({message: 'Invalid Token!'});
+
+  res.status(200).json({message: 'Valid Token!'})
+});
+
+app.post('/drinks', (req, res) => {
+  const { id, name, price } = req.body;
+  drinks.push({ id, name, price });
+  res.status(201).json({ message: 'Recipe created successfully!'})
+})
+
+
+app.put('/drinks/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const drinkIndex = drinks.findIndex((d) => d.id === Number(id));
+
+  if (drinkIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  drinks[drinkIndex] = { ...drinks[drinkIndex], name, price};
+
+  res.status(204).end();
+})
+
+app.delete('/drinks/:id', (req, res) => {
+  const { id } = req.params;
+  const drinkIndex = drinks.findIndex((d) => d.id === Number(id));
+
+  if (drinkIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  drinks.splice(drinkIndex, 1);
+
+  res.status(204).end();
+})
