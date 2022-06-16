0045264d7d7aad10a95cf002f0859b7f6bd8d5e8
Luskalves <lusca.alves32@gmail.com>
2022-06-16 10:54:04 -0300

    criando o middleware de erro

--- errorHandlerExample.js
+++ errorHandlerExample.js
@@ -1,16 +1,15 @@
 const express = require('express');
-const fs = require('fs');
+const rescue = require('express-rescue');
+const fs = require('fs/promises');
 
 const app = express();
 
-app.get('/:filename', async (req, res, next) => {
-    try {
-      const file = await fs.readFile(`./${req.params.filename}`);
-      res.send(file.toString('utf-8'))
-    } catch (e) {
-      next(e);
-    }
-})
+app.get('/:fileName', 
+  rescue(async (req, res) => {
+      const file = await fs.readFile(`./${req.params.fileName}`);
+      res.send(file.toString('utf-8'));
+  })
+);
 
 app.use(function (err, _req, res, _next) {
   res.status(500).json({ error: `ERROR: ${err.message}` })
