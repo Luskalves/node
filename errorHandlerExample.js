d428456513c7c5de5df15019a2e16fa6dc39eb8f
Luskalves <lusca.alves32@gmail.com>
2022-06-16 10:05:46 -0300

    criando handle de erro generico

---
+++ errorHandlerExample.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+app.get('/:filename', async (req, res, next) => {
+    try {
+      const file = await fs.readFile(`./${req.params.filename}`);
+      res.send(file.toString('utf-8'))
+    } catch (e) {
+      next(e);
+    }
+})
+
+app.use(function (err, _req, res, _next) {
+  res.status(500).json({ error: `ERROR: ${err.message}` })
+});
