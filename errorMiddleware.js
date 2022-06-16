5548c907e92a6b6fe521cf88ee06585ae292c067
Luskalves <lusca.alves32@gmail.com>
2022-06-16 11:01:20 -0300

    resolvendo erros de escrita

--- errorMiddleware.js
+++ errorMiddleware.js
@@ -1,6 +1,6 @@
 module.exports = (err, _req, res, _next) => {
   if (err.code && err.status) {
-    return res.ststus(err.status).json({ message: err.message, code: err.code });
+    return res.status(err.status).json({ message: err.message, code: err.code });
   }
   return res.status(500).jscon({ message: err.code });
 }
